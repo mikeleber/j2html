d318fba5a12cdeba7db93d293afca5ad3d485de8
mike_ <mikeleber71@googlemail.com>
2022-03-01 12:50:19 +0100

    add traverse method to DomContent

--- library/src/main/java/j2html/tags/DomContent.java
+++ library/src/main/java/j2html/tags/DomContent.java
@@ -1,8 +1,14 @@
 package j2html.tags;
 
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
 public abstract class DomContent implements Renderable {
     @Override
     public String toString() {
         return render();
+    }
+    void traverseTree(Consumer<DomContent> consumer, Predicate stopPredicate){
+
     }
 }
